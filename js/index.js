82b2c1797dd0feac3dc34fbf5ba9ddbe1d1aa79e
haddersbadders <hadders@dyn137103.shef.ac.uk>
2016-12-02 14:08:00 +0000

    Carousel changed
    Changed from Owl carousel to Slick Carousel.

--- js/index.js
+++ js/index.js
@@ -1,17 +1,8 @@
 // Uncomment to initialise WOW.js
 new WOW().init();
 
-// Owl Carousel initialisation
 $(document).ready(function(){
-  $(".owl-carousel").owlCarousel({
-    items: 1
+  $('.carousel').slick({
+    dots: true
   });
-  // This adds an additional carousel. Remove/comment out if not needed
-  // $(".alps-carousel").owlCarousel({
-  //   items: 1,
-  //   dots: false,
-  //   autoplay: true,
-  //   animateOut: 'fadeOut'
-  // }); //
-
 });

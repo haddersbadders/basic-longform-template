8f12f85ea0bd5c5fe3cb8ac8fdfd6c9267af99c7
Hadrian Cawthorne <haddersbadders@gmail.com>
2017-04-18 14:38:34 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -6,3 +6,20 @@ $(document).ready(function(){
     dots: true
   });
 });
+
+  var ukeVid = document.getElementById('uke');
+
+// init controller
+var controller = new ScrollMagic.Controller();
+
+// build scene
+var scene = new ScrollMagic.Scene({triggerElement: "#uke", duration: 200})
+        .addTo(controller)
+        .addIndicators() // add indicators (requires plugin)
+
+        .on("enter", function () {
+          ukeVid.play();
+        })
+        .on("leave", function () {
+          ukeVid.pause();
+        })
